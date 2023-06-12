fb19a3524fadbb69d56570391a0dc05e2304eece
rhakeogml00 <rhakeogml00@naver.com>
2023-06-12 13:10:50 +0900

    feat 아나그램

---
+++ src/section4/아나그램.java
@@ -0,0 +1,63 @@
+package section4;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class 아나그램 {
+    /*
+    설명
+
+    Anagram이란 두 문자열이 알파벳의 나열 순서를 다르지만 그 구성이 일치하면 두 단어는 아나그램이라고 합니다.
+
+    예를 들면 AbaAeCe 와 baeeACA 는 알파벳을 나열 순서는 다르지만 그 구성을 살펴보면 A(2), a(1), b(1), C(1), e(2)로
+
+    알파벳과 그 개수가 모두 일치합니다. 즉 어느 한 단어를 재 배열하면 상대편 단어가 될 수 있는 것을 아나그램이라 합니다.
+
+    길이가 같은 두 개의 단어가 주어지면 두 단어가 아나그램인지 판별하는 프로그램을 작성하세요. 아나그램 판별시 대소문자가 구분됩니다.
+
+
+    입력
+    첫 줄에 첫 번째 단어가 입력되고, 두 번째 줄에 두 번째 단어가 입력됩니다.
+
+    단어의 길이는 100을 넘지 않습니다.
+
+
+    출력
+    두 단어가 아나그램이면 “YES"를 출력하고, 아니면 ”NO"를 출력합니다.
+    */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next1 = scanner.next();
+//        int next2 = scanner.nextInt();
+
+//        int[] arr = new int[next1];
+//        for (int i = 0; i < next1; i++) {
+//            arr[i] = scanner.nextInt();
+//        }
+        String next2 = scanner.next();
+
+        String result = solution(next1, next2);
+        System.out.println(result);
+    }
+
+    public static String solution (String before, String after){
+        String answer = "";
+        // before의 단어를 재배치해서 after가 되면 아나그램
+
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        // before과 after의 문자별 개수를 구함
+        for (char c : before.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : after.toCharArray()) {
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+        }
+        // 두 map이 동일하면 아나그램
+        answer = map.equals(map2) ? "YES" : "NO";
+
+        return answer;
+    }
+}
